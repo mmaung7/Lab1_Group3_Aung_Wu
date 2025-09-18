5b4bae4c18d8dd85054495e1f94cc3a3178d2ce8
Myint Myat Aung <162494140+mmaung7@users.noreply.github.com>
2025-09-18 15:06:48 -0700

    Add files via upload

---
+++ main_menu.py
@@ -0,0 +1,18 @@
+import guessing_game
+
+def main():
+    print("Which game do you want to play?")
+    print("1. Guess the Number")
+    print("2. Rock, Paper, Scissors")
+    print("3. Exit")
+    choice = input("Enter the number of your choice: ")
+    if choice == "1":
+        guessing_game.guessing_game()
+    elif choice == "2":
+        pass
+    elif choice == "3":
+        print("Goodbye!")
+        exit()
+        
+if __name__ == "__main__":
+    main()
