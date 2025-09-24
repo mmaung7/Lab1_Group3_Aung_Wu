6533ab8bfbc2a2a99988d58ebc8cb61d512a38d5
captainmi618-droid <captainmi618@gmail.com>
2025-09-23 20:37:30 -0700

    Update rock_paper_scissors_game.py

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -2,9 +2,15 @@ import random
 import main_menu
 
 def rock_paper_scissors():
-  
-  print('Welcome to Rock-Paper-Scissors!')
-  print('Choose your move:')
-  print('1. Paper')
-  print('2. Scissors')
-  print('3. Rock')
+
+  while True:
+    print('Welcome to Rock-Paper-Scissors!')
+    print('Choose your move:')
+    print('1. Paper')
+    print('2. Scissors')
+    print('3. Rock')
+
+    try:
+      user_choice = int(input('Enter your choice (1, 2, 3): '))
+      if user_choice not in [1, 2, 3]:
+        
