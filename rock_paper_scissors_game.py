d7ba53886a14619cfd4f04bfc5d6980e31ed7eb4
captainmi618-droid <captainmi618@gmail.com>
2025-09-23 21:03:35 -0700

    Update rock_paper_scissors_game.py

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -13,4 +13,7 @@ def rock_paper_scissors():
     try:
       user_choice = int(input('Enter your choice (1, 2, 3): '))
       if user_choice not in [1, 2, 3]:
+        print("Invalid choice. Please enter 1, 2, or 3.")
+        continue
+      
         
