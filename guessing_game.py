3448cfbe9b3ff915201315008a5fa7507beb86b4
Myint Myat Aung <162494140+mmaung7@users.noreply.github.com>
2025-09-26 04:15:37 +0000

    Change to iteration not to make stack overflow if user runs too many times

--- guessing_game.py
+++ guessing_game.py
@@ -14,28 +14,32 @@ def guessing_game():
 
     Author: Myint Myat Aung
     """
-    target = random.randint(1, 100)
-    print("Welcome to the Guessing Game!")
-    print("I'm thinking of a number between 1 and 100.")
-    print("Guess what it is. You have 5 tries: ")
-    guess = input()
-    for i in range(4, 0, -1):
-        if int(guess) < target:
-            print("Your guess is too low. You have", i, "tries left.")
-            guess = input()
-        elif int(guess) > target:
-            print("Your guess is too high. You have", i, "tries left.")
-            guess = input()
-        else:
-           print("Congratulations! You've guessed the number!")
-           break
-        if i == 1 and int(guess) != target:
-            print("Sorry, you've used all your tries. The number was", target)
-    print("Do you want to play again? (yes/no)")
-    again = input().lower()
-    if again == "no":
-        main_menu.main()
-    elif again == "yes":
-        guessing_game()
+    play = True
+    while play == True:
+        target = random.randint(1, 100)
+        print("Welcome to the Guessing Game!")
+        print("I'm thinking of a number between 1 and 100.")
+        print("Guess what it is. You have 5 tries: ")
+        guess = input()
+        for i in range(4, 0, -1):
+            if int(guess) < target:
+                print("Your guess is too low. You have", i, "tries left.")
+                guess = input()
+            elif int(guess) > target:
+                print("Your guess is too high. You have", i, "tries left.")
+                guess = input()
+            else:
+                print("Congratulations! You've guessed the number!")
+                break
+            if i == 1 and int(guess) != target:
+                    print("Sorry, you've used all your tries. The number was", target)
+        print("Do you want to play again? (yes/no)")
+        again = input().lower()
+        if again == "no":
+            main_menu.main()
+            break
+        elif again == "yes":
+            play = True
+            
     
     
